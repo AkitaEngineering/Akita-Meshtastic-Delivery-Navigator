71b92bcf73eac9682ed01d0c3a0efbcd11938c58
sh4un <97253929+sh4un-dot-com@users.noreply.github.com>
2025-05-02 17:40:45 -0400

    Create __init__.py

---
+++ akita_navigator/__init__.py
@@ -0,0 +1,5 @@
+# Akita Meshtastic Delivery Navigator Package
+# Copyright (C) 2025 Akita Engineering <http://www.akitaengineering.com>
+# Licensed under GPLv3. See LICENSE file for details.
+
+# This file makes the akita_navigator directory a Python package.
